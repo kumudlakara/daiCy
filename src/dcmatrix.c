1611ffe69b2f5207bc5ab5c007bc71126f0cdbd1
kumudlakara <lakara.kumud@gmail.com>
2020-12-16 15:26:00 +0530

    Add docs for dcmat

--- src/dcmatrix.c
+++ src/dcmatrix.c
@@ -7,6 +7,15 @@
 
 ///////////////////////////////dcMatrix////////////////////////////////////
 void dcmatrix(dcmat *mat, int r, int c){
+	/*
+	Initialise a dcmatrix
+
+	Parameters
+	----------
+	mat: dcmat pointer
+	r : int number of rows 
+	c: int number of columns
+	*/
 	mat->data = (int**)calloc(r, sizeof(int*));
 	for(int i=0; i<r; i++)
 		mat->data[i] = (int*)calloc(c, sizeof(int));
@@ -15,9 +24,24 @@ void dcmatrix(dcmat *mat, int r, int c){
 }
 
 dcmat dcmatadd(dcmat *mat1, dcmat *mat2){
+	/*
+	Adds two dcmatrices and returns the resultant dcmatrix
+
+	Parameters
+	----------
+	mat1: dcmat pointer to first matrix
+	mat2: dcmat pointer to second matrix
+
+	Returns
+	-------
+	rmat: resultant matrix
+	*/
 	dcmat rmat;
 	if((mat1->r != mat2->r) || (mat1->c != mat2->c)){
 		//raise error and exit
+		printf("DaiCyError: dcMatrix dimensions not compatible");
+		exit(1);
+
 	}
 	rmat.r = mat1->r;
 	rmat.c = mat1->c;
@@ -31,9 +55,23 @@ dcmat dcmatadd(dcmat *mat1, dcmat *mat2){
 }
 
 dcmat dcmatsub(dcmat *mat1, dcmat *mat2){
+	/*
+	Subtracts two dcmatrices and returns the resultant dcmatrix
+
+	Parameters
+	----------
+	mat1: dcmat pointer to first matrix
+	mat2: dcmat pointer to second matrix
+
+	Returns
+	-------
+	rmat: resultant matrix
+	*/
 	dcmat rmat;
 	if((mat1->r != mat2->r) || (mat1->c != mat2->c)){
 		//raise error and exit
+		printf("DaiCyError: dcMatrix dimensions not compatible");
+		exit(1);
 	}
 	rmat.r = mat1->r;
 	rmat.c = mat1->c;
@@ -47,6 +85,18 @@ dcmat dcmatsub(dcmat *mat1, dcmat *mat2){
 } 
 
 dcmat dcmatones(int r, int c){
+	/*
+	Returns a dcmatrix of ones
+
+	Parameters
+	----------
+	r: int number of rows
+	c: int number of columns
+
+	Returns
+	-------
+	rmat: dcmat of ones of dimensions (r,c)
+	*/
 	dcmat rmat;
 	rmat.r = r;
 	rmat.c = c;
@@ -60,6 +110,18 @@ dcmat dcmatones(int r, int c){
 }
 
 dcmat dcmatzeros(int r, int c){
+	/*
+	Returns a dcmatrix of zeros
+
+	Parameters
+	----------
+	r: int number of rows
+	c: int number of columns
+
+	Returns
+	-------
+	rmat: dcmat of zeros of dimensions (r,c)
+	*/
 	dcmat rmat;
 	rmat.r = r;
 	rmat.c = c;
@@ -73,6 +135,17 @@ dcmat dcmatzeros(int r, int c){
 }
 
 dcmat dcmatcopy(dcmat *mat){
+	/*
+	Returns a copy of the dcmatrix
+
+	Parameters
+	----------
+	mat: dcmat pointer
+
+	Returns
+	-------
+	rmat: copy of the given dcmatrix 
+	*/
 	dcmat rmat;
 	rmat.r = mat->r;
 	rmat.c = mat->c;
@@ -86,6 +159,21 @@ dcmat dcmatcopy(dcmat *mat){
 }
 
 int dcmattrace(dcmat *mat){
+	/*
+	Returns trace of given square dcmatrix
+
+	Parameters
+	----------
+	mat: dcmat pointer
+
+	Returns
+	-------
+	trace: int value of the trace of the dcmatrix
+	*/
+	if(mat->r != mat->c){
+		printf("DaiCyError: Trace not found. Matrix dimensions do not match.\n");
+		exit(1);
+	}
 	int trace = 0;
 	for(int i=0; i<mat->r; i++)
 		for(int j=0; j<mat->c; j++)
@@ -95,6 +183,13 @@ int dcmattrace(dcmat *mat){
 }
 
 void dcmatshow(dcmat *mat){
+	/*
+	Displays the dcmatrix
+
+	Parameters
+	----------
+	mat: dcmat pointer
+	*/
 	for(int i=0; i<mat->r; i++){
 		for(int j = 0; j<mat->c; j++){
 			printf("%d ", mat->data[i][j]);
@@ -104,6 +199,23 @@ void dcmatshow(dcmat *mat){
 }
 
 dcmat todcmat(int **a, int r, int c){
+	/*
+	Converts a given 2-D array into a dcmatrix
+
+	Parameters
+	----------
+	a: pointer to 2-D int array
+	r: int number of rows
+	c: int number of columns
+
+	Returns
+	-------
+	rmat: dcmatrix
+
+	Note
+	----
+	Can do away with r and c fields by using sizeof
+	*/
 	dcmat rmat;
 	rmat.r = r;
 	rmat.c = c;
@@ -117,8 +229,22 @@ dcmat todcmat(int **a, int r, int c){
 }
 
 dcmat dcmatmul(dcmat *mat1, dcmat *mat2){
+	/*
+	Multiplies two dcmatrices
+
+	Parameters
+	----------
+	mat1: pointer to dcmat mat1
+	mat2: pointer to dcmat mat2
+
+	Returns
+	-------
+	rmat Resultant dcmatrix 
+	*/
 	if(mat1->c != mat2->r){
 		//raise error and return
+		printf("DaiCyError: dcMatrix dimensions not compatible");
+		exit(1);
 	}
 
 	dcmat rmat;
@@ -135,6 +261,17 @@ dcmat dcmatmul(dcmat *mat1, dcmat *mat2){
 }
 
 dcmat dcmattranspose(dcmat *mat){
+	/*
+	Returns the transpose of the given dcmatrix
+
+	Parameter
+	---------
+	mat: pointer to dcmat
+
+	Returns
+	-------
+	rmat: transpose of the dcmatrix
+	*/
 	dcmat rmat;
 	rmat.r = mat->c;
 	rmat.c = mat->r;
